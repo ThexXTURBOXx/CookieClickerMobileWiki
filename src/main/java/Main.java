ab020d49aa7b4fb57746e0c5560f3f945e6f7126
Nico Mexis <nico.mexis@kabelmail.de>
2023-12-14 16:36:47 +0100

    Fix order and a few more things

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,14 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import lombok.AllArgsConstructor;
 
 public class Main {
 
     private static final File FILE = new File("data_v7.23.js");
 
     public static void main(String[] args) throws IOException {
-        //printHeavenlyUpgrades();
+        printHeavenlyUpgrades();
         printAchievements();
         addExtraAchievementData();
     }
@@ -80,20 +81,19 @@ public class Main {
 
     private static final List<Achiev> ACHIEV_LIST = new ArrayList<>();
 
+    @AllArgsConstructor
     private static class Achiev implements Comparable<Achiev> {
         private int id;
+        private double order;
         private String name;
+        private String name2;
         private String desc;
-
-        public Achiev(int id, String name, String desc) {
-            this.id = id;
-            this.name = name;
-            this.desc = desc;
-        }
+        private boolean shadow;
 
         @Override
         public int compareTo(Achiev o) {
-            return Integer.compare(id, o.id);
+            if (shadow != o.shadow) return shadow ? 1 : -1;
+            return Double.compare(order, o.order);
         }
     }
 
@@ -104,9 +104,9 @@ public class Main {
         String nameStr = "";
         String idStr = "";
         String descStr = "";
+        String orderStr = "";
+        boolean shadow = false;
         boolean achievement = false;
-        boolean name = false;
-        boolean id = false;
         while ((line = reader.readLine()) != null) {
             line = line.trim();
             if (line.startsWith("new G.Achiev")) {
@@ -115,74 +115,89 @@ public class Main {
             }
             if (achievement && line.startsWith("name:")) {
                 nameStr = line.replaceFirst("name:\\s*`", "").replace("`,", "");
-                name = true;
                 continue;
             }
-            if (name && line.startsWith("id:")) {
+            if (achievement && line.startsWith("id:")) {
                 idStr = line.replaceFirst("id:", "").replace(",", "");
-                id = true;
                 continue;
             }
-            if (id && line.startsWith("desc:")) {
+            if (achievement && line.startsWith("desc:")) {
                 descStr = line.replaceFirst("desc:\\s*`", "").replace("`,", "");
                 continue;
             }
-            if (id && line.startsWith("});")) {
-                ACHIEV_LIST.add(new Achiev(Integer.parseInt(idStr), nameStr, descStr));
+            if (achievement && line.startsWith("order:")) {
+                orderStr = line.replaceFirst("order:\\s*", "").replace(",", "");
+                continue;
+            }
+            if (achievement && line.startsWith("pool:")) {
+                if (line.contains("shadow")) shadow = true;
+                continue;
+            }
+            if (achievement && line.startsWith("});")) {
+                ACHIEV_LIST.add(new Achiev(Integer.parseInt(idStr), Double.parseDouble(orderStr),
+                        nameStr, "", descStr, shadow));
                 nameStr = "";
                 idStr = "";
                 descStr = "";
+                orderStr = "";
+                shadow = false;
                 achievement = false;
-                id = false;
-                name = false;
                 continue;
             }
         }
-        //Collections.sort(ACHIEV_LIST);
+        Collections.sort(ACHIEV_LIST);
         for (Achiev a : ACHIEV_LIST)
             export.println(a.name + "|" + a.desc);
         export.flush();
         export.close();
     }
 
-        /*<td>Don't stop me now
-          </td>
-          <td>Bake <b>100 quintillion</b> cookies in one ascension.
-          </td>*/
-
     private static void addExtraAchievementData() throws IOException {
         PrintWriter export = new PrintWriter(Files.newOutputStream(Paths.get("achievementsExtended.txt")));
         for (Achiev a : ACHIEV_LIST) {
             String line;
-            File fileHtml = new File("welp.html");
-            BufferedReader readerHtml = new BufferedReader(new FileReader(fileHtml));
-            String name;
-            while (a.desc.isEmpty() && (line = readerHtml.readLine()) != null) {
-                line = line.trim();
-                if (!line.matches("^<tr.*>")) continue;
-                line = readerHtml.readLine().trim();
-                if (!line.matches("^<td.*>.+")) continue;
-                line = readerHtml.readLine().trim();
-                if (!line.matches("^</td.*>")) continue;
-                line = readerHtml.readLine().trim();
-                if (line.matches("^<td.*>.+")) {
-                    name = line.replaceFirst("<td.*?>", "");
-                    if (name.equalsIgnoreCase(a.name)) {
-                        line = readerHtml.readLine().trim();
-                        if (line.matches("^</td.*>")) {
-                            line = readerHtml.readLine().trim();
-                            if (line.matches("^<td.*>.+")) {
-                                a.desc = line.replaceFirst("<td.*?>", "");
-                                break;
+            File fileFixes = new File("fixes.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(fileFixes));
+            while (a.desc.isEmpty() && (line = reader.readLine()) != null) {
+                String[] split = (line.trim() + " ").split("\\|");
+                if (a.name.equalsIgnoreCase(split[0])) {
+                    a.name = split[1].isEmpty() ? a.name : split[1];
+                    a.name2 = split[2].isEmpty() ? a.name2 : split[2];
+                    a.desc = split[3].trim();
+                }
+            }
+            reader.close();
+            if (a.desc.isEmpty()) {
+                File fileHtml = new File("welp.html");
+                reader = new BufferedReader(new FileReader(fileHtml));
+                while (a.desc.isEmpty() && (line = reader.readLine()) != null) {
+                    line = line.trim();
+                    if (!line.matches("^<tr.*>")) continue;
+                    line = reader.readLine().trim();
+                    if (!line.matches("^<td.*>.+")) continue;
+                    line = reader.readLine().trim();
+                    if (!line.matches("^</td.*>")) continue;
+                    line = reader.readLine().trim();
+                    if (line.matches("^<td.*>.+")) {
+                        String name = line.replaceFirst("<td.*?>", "");
+                        if (name.equalsIgnoreCase(a.name) || name.equalsIgnoreCase(a.name2)) {
+                            line = reader.readLine().trim();
+                            if (line.matches("^</td.*>")) {
+                                line = reader.readLine().trim();
+                                if (line.matches("^<td.*>.+")) {
+                                    a.desc = line.replaceFirst("<td.*?>", "");
+                                }
                             }
                         }
                     }
                 }
+                reader.close();
             }
             if (a.desc.isEmpty()) {
                 System.out.println(a.name);
             }
-            export.println(a.name + " | " + a.desc);
+            a.desc = a.desc.replace("<div class=\"line\"></div>", "<br>");
+            export.println(a.name + (a.shadow ? " [SHADOW]" : "") + " | " + a.desc);
         }
         export.flush();
         export.close();
