84cbbfee789c153e91d01033ac8db68b29cc05dc
Nico Mexis <nico.mexis@kabelmail.de>
2023-12-31 13:06:57 +0100

    Allow achievement descFuncs
    if they ever get used

--- export.js
+++ export.js
@@ -1,2 +1,2 @@
-JSON.stringify(G.achievs, function(key, value) {if (key === 'unlockAt') { return undefined; } return value; });
+JSON.stringify(G.achievs.map((a) => { a.desc = a.descFunc?a.descFunc(a):a.desc; return a; }), function(key, value) {if (key === 'unlockAt') { return undefined; } return value; });
 JSON.stringify(G.upgrades.map((u) => { u.children = G.HeavenlyUpgradeChildren[u.id]; u.desc = u.descFunc?u.descFunc(u):u.desc; return u; }), function(key, value) {if (key === 'unlockAt' || key === 'unshackleUpgrade') { return undefined; } return value; });
