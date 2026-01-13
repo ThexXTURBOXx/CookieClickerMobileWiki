f754f6c4d5f04b1958150367262bdbca6fda6e6f
Nico Mexis <nico.mexis@kabelmail.de>
2026-01-13 19:55:41 +0100

    Fix problems I have found on a new PC

--- src/main/java/format/JSONFormatter.java
+++ src/main/java/format/JSONFormatter.java
@@ -2,14 +2,15 @@ package format;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.Strictness;
 import java.lang.reflect.Type;
 
 public class JSONFormatter {
 
-    private static final Gson GSON = new GsonBuilder().create();
+    private static final Gson GSON = new GsonBuilder().setStrictness(Strictness.LENIENT).create();
 
     public static String formatJSON(String json) {
-        if (json.startsWith("`")) json = json.substring(1, json.length() - 1);
+        if (json.startsWith("`")) json = json.substring(1, json.length() - 2);
         json = json.replace("\\\\\"", "\\\"");
         json = json.replace("<div style=\\\"display:inline-block;background:url(img/cookicon.png);width:16px;"
                             + "height:16px;\\\"></div>", "\uD83C\uDF6A");
